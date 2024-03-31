19f5019467a030472cf9ae8fb346d33b2137914a
SiddharthKS37 <165563775+SiddharthKS37@users.noreply.github.com>
2024-03-31 20:02:52 +0530

    Create billng.h

---
+++ billng.h
@@ -0,0 +1,11 @@
+#ifndef BILLING_H
+#define BILLING_H
+
+#include "bill_generation.h"
+#include "item_details.h"
+#include "customer_details.h"
+#include "total_bill_calculation.h"
+
+void run_billing_system();
+
+#endif
